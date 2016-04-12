652aa9e384eca2f07778e712756d4d73dd1eec59
Chandan Singh <phoenix.com005@gmail.com>
2016-04-12 23:04:59 +1000

    done

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,7 +16,7 @@ module SessionsHelper
 	# Return the corresponding user to current logged in user
 	def current_user
 		if (user_id = session[:user_id])
-			@current_user || = User.find_by(id: user_id)
+			@current_user ||= User.find_by(id: user_id)
 		elsif (user_id = cookies.signed[:user_id])
 			user = User.find_by(id: user_id)
 			if user && user.authenticated?(cookies[:remember_token])
